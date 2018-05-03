9abba8b1b98bfc8cccd042a6adb881bf14946b5f
cwx2810 <cwx2810@163.com>
2018-05-03 20:54:41 +0800

    单元测试

--- src/test/java/com/imooc/ApplicationTests.java
+++ src/test/java/com/imooc/ApplicationTests.java
@@ -7,5 +7,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
-
+    //总的测试用例，在下面写具体测试用例继承这个
 }
--- src/test/java/com/imooc/entity/UserRepositoryTest.java
+++ src/test/java/com/imooc/entity/UserRepositoryTest.java
@@ -2,16 +2,20 @@ package com.imooc.entity;
 
 import com.imooc.ApplicationTests;
 import com.imooc.repository.UserRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserRepositoryTest extends ApplicationTests {
+    //注入我们要测试的jpa
     @Autowired
     private UserRepository userRepository;
 
     @Test
     public void testFindOne() {
-        //User user = userRepository.findOne();
-
+        //找到id=1的user
+        User user = userRepository.findOne(1L);
+        //判断name是否等于我们在数据库已经插入的waliwali
+        Assert.assertEquals("waliwali",user.getName());
     }
 }
