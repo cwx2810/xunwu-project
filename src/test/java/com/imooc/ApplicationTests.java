308623aad7a645596798a97cbf5e9038e60e8b89
cwx2810 <cwx2810@163.com>
2018-05-03 21:10:22 +0800

    内存数据库测试h2集成

--- src/test/java/com/imooc/ApplicationTests.java
+++ src/test/java/com/imooc/ApplicationTests.java
@@ -2,10 +2,14 @@ package com.imooc;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Configuration
+@ActiveProfiles("test")
 public class ApplicationTests {
     //总的测试用例，在下面写具体测试用例继承这个
 }
